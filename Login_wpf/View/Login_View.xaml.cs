7d6752b6a0bc3011be8c334ff832a88654fd83de
Atari96 <pmorato10@yahoo.es>
2025-01-19 19:24:42 +0100

    Agregar archivos de proyecto.

---
+++ Login_wpf/View/Login_View.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Login_wpf.View
+{
+    /// <summary>
+    /// Lógica de interacción para Login_View.xaml
+    /// </summary>
+    public partial class Login_View : Window
+    {
+        public Login_View()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton==MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
